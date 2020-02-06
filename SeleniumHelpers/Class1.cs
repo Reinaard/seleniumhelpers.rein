83701fce5abea8eaf68c9af3ac59ddba7f7596fd
Rein van der Vegt <>
2020-02-06 23:38:43 +0100

    Initial

---
+++ SeleniumHelpers/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumHelpers
+{
+    public class Class1
+    {
+        public void DoOperation()
+        {
+            Console.WriteLine("Called DoOperation");
+        }
+    }
+}
